476123efcc813be34067851ff8d1e96c6b7811e8
Иван Олинов <IVOlinov@sberbank.ru>
2023-03-03 13:12:54 +0300

    F-0: Подключить less

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -66,9 +66,9 @@ const swSrc = paths.swSrc
 
 // style files regexes
 const cssRegex = /\.css$/
+const lessRegex = /\.less$/
 const cssModuleRegex = /\.module\.css$/
-const sassRegex = /\.(scss|sass)$/
-const sassModuleRegex = /\.module\.(scss|sass)$/
+const lessModuleRegex = /\.module\.less$/
 
 const hasJsxRuntime = (() => {
 	if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
@@ -504,33 +504,26 @@ module.exports = function (webpackEnv) {
 						// By default we support SASS Modules with the
 						// extensions .module.scss or .module.sass
 						{
-							test: sassRegex,
-							exclude: sassModuleRegex,
-							use: getStyleLoaders(
-								{
-									importLoaders: 3,
-									sourceMap: isEnvProduction
-										? shouldUseSourceMap
-										: isEnvDevelopment,
-									modules: {
-										mode: 'icss',
-									},
+							test: lessRegex,
+							exclude: lessModuleRegex,
+							use: getStyleLoaders({
+								importLoaders: 1,
+								sourceMap: isEnvProduction
+									? shouldUseSourceMap
+									: isEnvDevelopment,
+								modules: {
+									mode: 'icss',
 								},
-								'sass-loader'
-							),
-							// Don't consider CSS imports dead code even if the
-							// containing package claims to have no side effects.
-							// Remove this when webpack adds a warning or an error for this.
-							// See https://github.com/webpack/webpack/issues/6571
+							}),
 							sideEffects: true,
 						},
 						// Adds support for CSS Modules, but using SASS
 						// using the extension .module.scss or .module.sass
 						{
-							test: sassModuleRegex,
+							test: lessModuleRegex,
 							use: getStyleLoaders(
 								{
-									importLoaders: 3,
+									importLoaders: 1,
 									sourceMap: isEnvProduction
 										? shouldUseSourceMap
 										: isEnvDevelopment,
@@ -539,7 +532,7 @@ module.exports = function (webpackEnv) {
 										getLocalIdent: getCSSModuleLocalIdent,
 									},
 								},
-								'sass-loader'
+								'less-loader'
 							),
 						},
 						// "file" loader makes sure those assets get served by WebpackDevServer.
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,44 @@
-import './App.css'
+import '@vkontakte/vkui/dist/vkui.css'
 
-import React from 'react'
+import bridge from '@vkontakte/vk-bridge'
+import {
+	AdaptivityProvider,
+	AppRoot,
+	ConfigProvider,
+	ScreenSpinner,
+	SplitCol,
+	SplitLayout,
+	View,
+} from '@vkontakte/vkui'
+import React, { useEffect, useState } from 'react'
 
-function App() {
-	return <div className='App'></div>
+import { Directories } from './components/Directories/Directories'
+
+const App = () => {
+	const [activePanel, setActivePanel] = useState('directories')
+	const [user, setUser] = useState(null)
+
+	useEffect(() => {
+		fetchData()
+	}, [])
+
+	async function fetchData() {
+		const user = await bridge.send('VKWebAppGetUserInfo')
+		// @ts-ignore
+		setUser(user)
+	}
+
+	return (
+		<ConfigProvider>
+			<AdaptivityProvider>
+				<AppRoot>
+					<View activePanel={activePanel}>
+						<Directories id='directories' />
+					</View>
+				</AppRoot>
+			</AdaptivityProvider>
+		</ConfigProvider>
+	)
 }
 
 export default App
